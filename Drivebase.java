a86b66dbc33912960fdd119b71c4ff48dc7fb6c5
PokaDoka1 <90730806+PokaDoka1@users.noreply.github.com>
2022-12-21 12:48:09 -0800

    Update Drivebase.java

--- Drivebase.java
+++ Drivebase.java
@@ -4,59 +4,52 @@
 
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.SparkMaxAlternateEncoder;
-
+import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivebase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  public Drivebase() {}
 
-  private final int diamater = 1000; 
+  private final int diamater = 100;
 
-  CANSparkMax m_leftMotor = new CANSparkMax(0, MotorType.kBrushed);
-  CANSparkMax m_rightMotor = new CANSparkMax(3, MotorType.kBrushed);
+  //className objectName = new classConstructor(arguments);
+
+  CANSparkMax m_leftMotor = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax m_rightMotor = new CANSparkMax(2, MotorType.kBrushless);
 
   DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
-  public Drivebase() {
-    m_leftMotor.setInverted(true);
+  RelativeEncoder m_leftEncoder = m_leftMotor.getAlternateEncoder(Type.kQuadrature, 20);
+  RelativeEncoder m_rightEncoder = m_rightMotor.getAlternateEncoder(Type.kQuadrature, 20);
 
-    SparkMaxAlternateEncoder m_leftEncoder = m_leftMotor.getAlternateEncoder​(Type.kQuadrature, 20);
-    SparkMaxAlternateEncoder m_rightEncoder = m_rightMotor.getAlternateEncoder​(Type.kQuadrature, 20);  
+  public void arcadeDrive(double forwardOrBackwardSpeed, double turnSpeed){
+    //objectName.methodName(arguments);
+    m_drive.arcadeDrive(forwardOrBackwardSpeed, turnSpeed);
   }
 
-  public void arcadeDrive(double xSpeed, double rotationSpeed){
-    m_drive.arcadeDrive(xSpeed, rotationSpeed);
-  }
-
-  public void resetEncoders(double xSpeed, double rotationSpeed){
+  public void resetEncoders(){
     m_leftEncoder.setPosition(0);
     m_rightEncoder.setPosition(0);
   }
 
   public double getLeftDistanceInch(){
-    return m_leftEncoder.getPosition() * diameter * Math.PI;
+    return (m_leftEncoder.getPosition() * diamater * Math.PI);
   }
 
   public double getRightDistanceInch(){
-    return m_rightEncoder.getPosition() * diameter * Math.PI;
-  }
-
-  public double getDistanceInch(SparkMaxAlternateEncoder encoder){
-    return encoder.getPosition() * diameter * Math.PI;
-  }
-
-  public void getAverageDistanceInch(){
-    ((getLeftDistanceInch() + getRightDistanceInch()) /2 ) * diameter * Math.PI;
+    return (m_rightEncoder.getPosition() * diamater * Math.PI);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
